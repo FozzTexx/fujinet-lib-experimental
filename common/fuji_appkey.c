2e2a2130f71fd801641d4b7aacd79aa8d4bc4df2
Chris Osborn <fozztexx@fozztexx.com>
2025-07-07 08:45:47 -0700

    Use structs for appkey data instead of crazy casts.

--- common/fuji_appkey.c
+++ common/fuji_appkey.c
@@ -1,23 +1,28 @@
 #include "fujinet-fuji.h"
 #include <string.h>
 
-#define APPKEY_SIZE 64
+#define APPKEY_BLOCK_SIZE 64 // FIXME - use ak_appkey_size somehow
 
 static uint16_t ak_creator_id;
 static uint8_t ak_app_id;
 static enum AppKeySize ak_appkey_size;
 
-typedef struct
-{
+typedef struct {
   uint16_t creator;
   uint8_t app_id;
   uint8_t key_id;
   uint8_t mode;
   uint8_t reserved;
-} FNAppKey;
+} FNAppKeyID;
 
-static FNAppKey appkey;
-static uint8_t appkey_buf[APPKEY_SIZE + 4];
+typedef struct {
+  uint16_t block_size;
+  uint16_t length;
+  uint8_t data[APPKEY_BLOCK_SIZE];
+} FNAppKeyData;
+
+static FNAppKeyID appkey;
+static FNAppKeyData appkey_buf;
 
 static void init_appkey(uint8_t key_id, uint8_t mode)
 {
@@ -34,25 +39,32 @@ static void init_appkey(uint8_t key_id, uint8_t mode)
 
 bool fuji_read_appkey(uint8_t key_id, uint16_t *count, uint8_t *data)
 {
+  FNAppKeyData *ak_data = (FNAppKeyData *) data;
+
+
   init_appkey(key_id, 0);
   if (!FUJICALL_D(FUJICMD_OPEN_APPKEY, &appkey, sizeof(appkey)))
     return false;
 
-  if (!FUJICALL_RV(FUJICMD_READ_APPKEY, data, APPKEY_SIZE + 4))
+  if (!FUJICALL_RV(FUJICMD_READ_APPKEY, ak_data, sizeof(FNAppKeyData)))
     return false;
-  *count = *(uint16_t *) &data[2];
-  memmove(data, &data[4], APPKEY_SIZE);
+  *count = ak_data->length;
+  memmove(data, ak_data->data, sizeof(ak_data->data));
   return true;
 }
 
 bool fuji_write_appkey(uint8_t key_id, uint16_t count, uint8_t *data)
 {
+  FNAppKeyData *ak_data = (FNAppKeyData *) data;
+
+
   init_appkey(key_id, 1);
   if (!FUJICALL_D(FUJICMD_OPEN_APPKEY, &appkey, sizeof(appkey)))
     return false;
-  *((uint16_t *) appkey_buf) = count;
-  memcpy(&appkey_buf[2], data, APPKEY_SIZE);
-  return FUJICALL_D(FUJICMD_WRITE_APPKEY, appkey_buf, sizeof(appkey_buf));
+  appkey_buf.length = count;
+  memcpy(appkey_buf.data, data, APPKEY_BLOCK_SIZE);
+  return FUJICALL_D(FUJICMD_WRITE_APPKEY, &appkey_buf.length,
+                    sizeof(appkey_buf) - sizeof(appkey_buf.block_size));
 }
 
 void fuji_set_appkey_details(uint16_t creator_id, uint8_t app_id, enum AppKeySize keysize)
