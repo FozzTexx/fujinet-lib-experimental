03cc3969e8c1b4871150118497bfb569430a7f12
Chris Osborn <fozztexx@fozztexx.com>
2025-08-28 09:12:31 -0700

    Added include/fujinet-int.h

---
+++ include/fujinet-int.h
@@ -0,0 +1,14 @@
+#ifndef FUJINET_INT_H
+#define FUJINET_INT_H
+
+#ifdef _CMOC_VERSION_
+    #include <cmoc.h>
+    #include <coco.h>
+    #include "stdbool-coco.h"
+#else
+    #include <stddef.h>
+    #include <stdbool.h>
+    #include <stdint.h>
+#endif /* _CMOC_VERSION_ */
+
+#endif /* FUJINET_INT_H */
