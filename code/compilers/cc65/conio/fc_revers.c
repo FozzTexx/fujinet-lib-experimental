870534a95298720e4a69a84e6ecb46c455ce73d0
shadowbreeze7881 <theretrolife@soulexi.com>
2026-03-13 02:16:52 -0400

    Added two new functions: fc_chlinexy and fc_revers

---
+++ code/compilers/cc65/conio/fc_revers.c
@@ -0,0 +1,6 @@
+#include <conio.h>
+#include "fujinet-compiler.h"
+
+unsigned char fc_revers(unsigned char onoff) {
+    return revers(onoff);
+}
---
+++ code/compilers/z88dk/conio/fc_revers.c
@@ -0,0 +1,10 @@
+#include <conio.h>
+#include "fujinet-compiler.h"
+
+unsigned char fc_revers(unsigned char onoff) {
+    //return revers(onoff);
+
+    // Ignore, for now.  Many z80 machines does
+    //   not have the capability of doing inverse characters.
+    return 0;
+}
