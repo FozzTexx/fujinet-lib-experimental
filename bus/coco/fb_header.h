d9b41ce79b6e9f7e6ee39247e035d2a935b470ea
Chris Osborn <fozztexx@fozztexx.com>
2026-07-01 10:01:42 -0700

    Split up fujinet-bus-coco.c

---
+++ bus/coco/fb_header.h
@@ -0,0 +1,19 @@
+#ifndef FB_HEADER_H
+#define FB_HEADER_H
+
+#include <fujinet-int.h>
+
+typedef struct {
+  uint8_t opcode;
+  union {
+    uint8_t cmd;
+    struct {
+      uint8_t unit;
+      uint8_t cmd;
+    } net;
+  } fn;
+} fujibus_header;
+
+extern fujibus_header fb_header;
+
+#endif /* FB_HEADER_H */
