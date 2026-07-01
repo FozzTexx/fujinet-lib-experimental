47da3be19a58a28c0e6e83a3aa37899b45481975
Chris Osborn <fozztexx@fozztexx.com>
2026-07-01 09:14:34 -0700

    Add appkey vars

--- common/fuji_appkey.c
+++ common/fuji_appkey.c
@@ -3,6 +3,11 @@
 #include <fujinet-appkey.h>
 #include <fujinet-fuji.h>
 
+uint16_t ak_creator_id;
+uint8_t ak_app_id;
+enum AppKeySize ak_appkey_size;
+FNAppKeyID appkey;
+
 static void init_appkey(uint8_t key_id, uint8_t mode)
 {
   uint8_t idx = 0;
