6507ba7f7bc7c4b59562cf3381ad2bf5bed299df
Chris Osborn <fozztexx@fozztexx.com>
2026-07-02 06:46:53 -0700

    Integrate fuji_net_call and pack_payload into fuji_bus_call

--- bus/coco/fuji_bus_call.c
+++ bus/coco/fuji_bus_call.c
@@ -6,6 +6,8 @@
 #include <fujinet-int.h>
 #include <fujinet-bus.h>
 
+fujibus_header fb_header;
+
 bool fuji_bus_call(uint8_t device, uint8_t fuji_cmd, uint8_t fields,
 		   uint8_t aux1, uint8_t aux2, uint8_t aux3, uint8_t aux4,
 		   const void *data, size_t data_length,
