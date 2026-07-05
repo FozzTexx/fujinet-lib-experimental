2b1daae4c794c051cd6c629cd90ce57505e6a3ac
Chris Osborn <fozztexx@fozztexx.com>
2026-07-05 08:55:20 -0700

    Convert Apple II to use varargs

--- bus/apple2/fujinet-bus-apple2.c
+++ bus/apple2/fujinet-bus-apple2.c
@@ -3,8 +3,7 @@
 #include "fujinet-network.h"
 #include "fujinet-err.h"
 #include <string.h>
-
-#include <stdio.h> // debug
+#include <stdarg.h>
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
@@ -16,14 +15,12 @@ typedef struct {
 } fujibus_packet;
 
 static fujibus_packet *fb_packet = (fujibus_packet *) sp_payload;
-bool fuji_bus_call(uint8_t device, uint8_t fuji_cmd, uint8_t fields,
-		   uint8_t aux1, uint8_t aux2, uint8_t aux3, uint8_t aux4,
-		   const void *data, size_t data_length,
-		   void *reply, size_t reply_length)
+bool fuji_bus_call(uint8_t device, uint8_t fuji_cmd, uint8_t fields, ...)
 {
   bool is_status;
   uint8_t sp_id;
   uint16_t idx, numbytes;
+  va_list ap;
 
 
   if (device == FUJI_DEVICEID_FUJINET)
@@ -54,31 +51,26 @@ bool fuji_bus_call(uint8_t device, uint8_t fuji_cmd, uint8_t fields,
   if (sp_id == 0)
     return false;
 
-  // FIXME - I think there's a couple of commands that don't have a
-  //         reply value but used SP_STATUS instead of SP_CONTROL
-  is_status = !!reply;
-  //printf("COMMAND: 0x%02x:%02x IS STATUS: %i\n", device, fuji_cmd, is_status);
+  is_status = fields & FUJI_FIELD_REPLY;
+
+  va_start(ap, fields);
 
   if (fields || !is_status) {
     idx = 0;
     numbytes = fuji_field_numbytes(fields);
-    if (numbytes) {
-      fb_packet->data[idx++] = aux1;
-      numbytes--;
-    }
-    if (numbytes) {
-      fb_packet->data[idx++] = aux2;
-      numbytes--;
-    }
-    if (numbytes) {
-      fb_packet->data[idx++] = aux3;
-      numbytes--;
-    }
-    if (numbytes) {
-      fb_packet->data[idx++] = aux4;
-      numbytes--;
-    }
-    if (data) {
+    if (numbytes > 0)
+      fb_packet->data[idx++] = va_arg(ap, uint8_t);
+    if (numbytes > 1)
+      fb_packet->data[idx++] = va_arg(ap, uint8_t);
+    if (numbytes > 2)
+      fb_packet->data[idx++] = va_arg(ap, uint8_t);
+    if (numbytes > 3)
+      fb_packet->data[idx++] = va_arg(ap, uint8_t);
+    if (fields & FUJI_FIELD_DATA) {
+      const uint8_t *data = va_arg(ap, uint8_t *);
+      const uint16_t data_length = va_arg(ap, uint16_t);
+
+
       memcpy(&fb_packet->data[idx], data, data_length);
       idx += data_length;
     }
@@ -96,12 +88,18 @@ bool fuji_bus_call(uint8_t device, uint8_t fuji_cmd, uint8_t fields,
       sp_status_nw(sp_id, fuji_cmd);
     else
       sp_status(sp_id, fuji_cmd);
-    if (!sp_error && reply) {
+    if (!sp_error && (fields & FUJI_FIELD_REPLY)) {
+      uint8_t *reply = va_arg(ap, uint8_t *);
+      uint16_t reply_length = va_arg(ap, uint16_t);
+
+
       if (sp_count < reply_length)
         reply_length = sp_count;
       memcpy(reply, &sp_payload[0], reply_length);
     }
   }
+
+  va_end(ap);
 
   return !sp_error;
 }
--- common/fuji_read_directory.c
+++ common/fuji_read_directory.c
@@ -1,6 +1,6 @@
 #include <fujinet-fuji.h>
 
-bool fuji_read_directory(uint8_t maxlen, uint8_t aux2, char *buffer)
+bool fuji_read_directory(uint8_t maxlen, uint8_t aux2, void *buffer)
 {
   return FUJICALL_A1_A2_RV(FUJICMD_READ_DIR_ENTRY, maxlen, aux2, buffer, maxlen);
 }
