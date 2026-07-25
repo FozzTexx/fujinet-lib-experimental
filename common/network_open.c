87662d51894a5e427211dc5e6b5a2cdc09b67ec7
Chris Osborn <fozztexx@fozztexx.com>
2026-07-25 08:47:47 -0700

    Adam is variable length

--- common/network_open.c
+++ common/network_open.c
@@ -14,7 +14,7 @@ FN_ERR network_open(const char *devicespec, uint8_t mode, uint8_t trans)
   uint8_t nw_unit = network_unit(devicespec);
 
 
-  if (!NETCALL_A1_A2_D(FUJICMD_OPEN, nw_unit, mode, trans, devicespec, MAX_FILENAME_LEN))
+  if (!NETCALL_A1_A2_D(FUJICMD_OPEN, nw_unit, mode, trans, devicespec, NETWORK_OPEN_LEN))
     return FN_ERR_IO_ERROR;
 
 #if 0
