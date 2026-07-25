73ef498e7a3fc9779ae848d55d80c58536e2aa8b
Chris Osborn <fozztexx@fozztexx.com>
2026-07-25 08:27:16 -0700

    Remove unused ifdef

--- common/fuji_set_directory_position.c
+++ common/fuji_set_directory_position.c
@@ -2,10 +2,5 @@
 
 bool fuji_set_directory_position(uint16_t pos)
 {
-#if 0 //def _CMOC_VERSION_
-#warning "A1_A2 shouldn't be necessary"
-  return FUJICALL_A1_A2(FUJICMD_SET_DIRECTORY_POSITION, U16_MSB(pos), U16_LSB(pos));
-#else
   return FUJICALL_B12(FUJICMD_SET_DIRECTORY_POSITION, pos);
-#endif
 }
