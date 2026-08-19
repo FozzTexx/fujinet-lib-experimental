f1bc57e71229020e5cd6ccfe412095159f8ff5aa
Rich Stephens <richswww@gmail.com>
2026-08-19 14:31:12 -0400

    [testing] Make coco3 screen colors white on black.

--- testing/src/init_video.c
+++ testing/src/init_video.c
@@ -12,8 +12,16 @@ void init_video()
 {
 #if defined(BUILD_COCO)
   initCoCoSupport();
-  if (isCoCo3)
+  if (isCoCo3) {
     width(80);
+    /* Text takes its background from palette slots 0-7 and its foreground
+     * from 8-15, so both ends have to be set to override the default. */
+    palette(0, 0);
+    palette(8, 63);
+    setBorderColor(0);
+    attr(0, 0, 0, 0);
+    cls(0);
+  }
 #elif defined(BUILD_APPLE2)
   videomode(VIDEOMODE_80COL);
 #endif // _CMOC_VERSION_
