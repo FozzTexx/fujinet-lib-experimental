bf8c7997189d83be44b0932f49f3e576d569e140
Chris Osborn <fozztexx@fozztexx.com>
2026-08-23 06:39:54 -0700

    [adam] Add hack to allow more retries on WiFi scan
    `fuji_bus_call()` really shouldn't be special casing FUJICMD_COPY_FILE
    and FUJICMD_SCAN_NETWORKS, there should be special Adam versions of
    those functions to deal with the longer wait time.

--- testing/tests/broken-v5.h
+++ testing/tests/broken-v5.h
@@ -7,10 +7,6 @@
 
 #if defined(BUILD_ADAM) || defined(BUILD_ADAM_CPM)
 // Missing from firmware
-#define FN_BROKEN_fuji_status
-#define FN_BROKEN_fuji_get_host_prefix
-#define FN_BROKEN_fuji_get_wifi_enabled
-#define FN_BROKEN_fuji_scan_for_networks
 #define FN_BROKEN_network_set_eol
 
 // adamnet has no clock device; only the Fuji device's GET TIME exists
