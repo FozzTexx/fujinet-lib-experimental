661a98a9351ace13552f47c616522f2622bb8110
Rich Stephens <richswww@gmail.com>
2026-08-24 14:01:00 -0400

    Roll back "broken" on c64 clock tests

--- testing/tests/broken-v5.h
+++ testing/tests/broken-v5.h
@@ -14,20 +14,6 @@
 #define FN_BROKEN_network_set_eol
 #endif /* BUILD_ADAM || BUILD_ADAM_CPM */
 
-#ifdef BUILD_C64
-// iec has no fujiClock device; the clock at device 29 is stream-based only
-#define FN_BROKEN_clock_get_time_APETIME_BINARY
-#define FN_BROKEN_clock_get_time_APPLE3_SOS_BINARY
-#define FN_BROKEN_clock_get_time_PRODOS_BINARY
-#define FN_BROKEN_clock_get_time_SIMPLE_BINARY
-#define FN_BROKEN_clock_get_time_SIMPLE_BINARY_WITH_HUNDREDTHS
-#define FN_BROKEN_clock_get_time_tz
-#define FN_BROKEN_clock_get_time_TZ_ISO_STRING
-#define FN_BROKEN_clock_get_time_UTC_ISO_STRING
-#define FN_BROKEN_clock_get_tz
-#define FN_BROKEN_clock_set_tz
-#endif /* BUILD_C64 */
-
 #ifdef BUILD_COCO
 // Missing from firmware
 #define FN_BROKEN_network_set_eol
